00c411bd5aeaee95c109bcb22b6253dbefae0594
aldegad <aldegad@naver.com>
2021-11-26 23:04:05 +0900

    datetime doing

--- src/app/basic/component/input/datetime/datetime.component.ts
+++ src/app/basic/component/input/datetime/datetime.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-datetime',
   templateUrl: './datetime.component.html',
   styleUrls: ['./datetime.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => DatetimeComponent),
+    multi: true
+  }]
 })
-export class DatetimeComponent implements OnInit {
+export class DatetimeComponent implements OnInit, ControlValueAccessor {
 
   constructor() { }
 
   ngOnInit() {}
 
+  //default setting
+  @Input() readonly:boolean = false;
+  @Input() disabled:boolean = false;
+  @Output() change = new EventEmitter();
+
+  public _value:boolean = false;
+  @Input()
+  set value(v:any) {
+    if(v !== this.value) {
+      this._value = v === this.on ? true : false;
+    }
+  }
+  get value() { return this._value ? this.on : this.off; }
+  
+  writeValue(v:any): void {
+    console.log(v);
+    if(v !== this._value) this._value = v; 
+  }
+  private _onChangeCallback = (v) => {};
+  private _onTouchedCallback = (v) => {};
+  registerOnChange(fn: any): void { this._onChangeCallback = fn; }
+  registerOnTouched(fn: any): void { this._onTouchedCallback = fn; }
 }
