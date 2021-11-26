1c6d94b325517f2f64ae27bc14f8d25036c133f6
aldegad <aldegad@naver.com>
2021-11-26 16:16:39 +0900

    check color fix

--- src/app/basic/component/input/check/check.component.ts
+++ src/app/basic/component/input/check/check.component.ts
@@ -18,6 +18,7 @@ export class CheckComponent implements OnInit, ControlValueAccessor {
 
   @HostBinding('class') get class() {
     let _class = [];
+    if(this.color) _class.push(`ion-color-${this.color}`);
     if(this.disabled) _class.push(`checkbox-disabled`);
     if(this.readonly) _class.push(`checkbox-readonly`);
     return _class.join(' ');
