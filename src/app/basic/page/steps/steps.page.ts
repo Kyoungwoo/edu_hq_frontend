98668ee8f3b84f1cbb34e3a872ef23f082475adc
hyewon1234 <heywon303@naver.com>
2021-11-26 17:42:05 +0900

    steps

--- src/app/basic/page/steps/steps.page.ts
+++ src/app/basic/page/steps/steps.page.ts
@@ -9,9 +9,46 @@ export class StepsPage implements OnInit {
 
   nav = '';
 
+  
+  steps = [
+    {
+      number: 1,
+      text: '회원유형 선택',
+      check: true
+    },
+    {
+      number: 2,
+      text: '기본정보 입력',
+      check: true
+    },
+
+    {
+      number: 3,
+      text: '건강문진 작성',
+      check: true
+    },
+    {
+      number: 3,
+      text: '약관동의',
+      check: true
+    },
+    {
+      number: 3,
+      text: '가입완료',
+      check: true
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  check(item){
+    if(item.check) {
+      item.check = false;
+    } else {
+      item.check = true;
+    }
+  }
 }
