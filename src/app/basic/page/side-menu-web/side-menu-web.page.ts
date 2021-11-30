258deda811104eb32485df859a2f51f96865260e
hyewon1234 <heywon303@naver.com>
2021-11-30 18:03:17 +0900

    sidemenu web'

--- src/app/basic/page/side-menu-web/side-menu-web.page.ts
+++ src/app/basic/page/side-menu-web/side-menu-web.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInAnimation } from '../../app.animation';
 
 @Component({
   selector: 'app-side-menu-web',
   templateUrl: './side-menu-web.page.html',
   styleUrls: ['./side-menu-web.page.scss'],
+  animations: [ fadeInAnimation ]
 })
 export class SideMenuWebPage implements OnInit {
 
+  document:boolean = false;
+  sign:boolean = false;
+  risk:boolean = false;
+  smart:boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
   }
-
 }
