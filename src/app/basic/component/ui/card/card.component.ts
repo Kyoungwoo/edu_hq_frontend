71c98d2ba64cd4453e705aaa0989b44247b24cc3
aldegad <aldegad@naver.com>
2021-12-02 23:50:16 +0900

    card add translucent fill

--- src/app/basic/component/ui/card/card.component.ts
+++ src/app/basic/component/ui/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import { Color } from '@ionic/core';
 
 @Component({
@@ -8,8 +8,15 @@ import { Color } from '@ionic/core';
 })
 export class CardComponent implements OnInit {
 
+  @HostBinding('class') get class() {
+    let _class = [];
+    if(this.fill) _class.push(`card-${this.fill}`);
+    return _class.join(' ');
+  }
+
   @Input() button:boolean = true;
   @Input() color:Color = 'white';
+  @Input() fill:'translucent' | 'translucent-outline';
   @Input() href:string;
   @Input() download:string;
 
