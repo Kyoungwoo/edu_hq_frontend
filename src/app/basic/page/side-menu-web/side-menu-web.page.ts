3243db9d735d7f57aeec37810c4aee4d6dd38b16
hyewon1234 <heywon303@naver.com>
2021-12-07 11:51:31 +0900

    alert

--- src/app/basic/page/side-menu-web/side-menu-web.page.ts
+++ src/app/basic/page/side-menu-web/side-menu-web.page.ts
@@ -147,10 +147,11 @@ export class SideMenuWebPage implements OnInit {
     // })
 
     // test_modal.present();
+
     const alert = await this.alert.create({
-      message:"<img src='assets/basic/img/close.svg'/>",
+      message:"<img src='assets/basic/img/work-plan.svg'/>",
       header:'회원가입이 반려되었습니다.',
-      subHeader: '정보 수정 후 재가입 신청 하시겠습니까? 반려사유 : 정보 오류',
+      subHeader: '정보 수정 후 재가입 신청 하시겠습니까?\n반려사유 : 정보 오류',
       cssClass:'alertButton',
       buttons:[
         {text:'정보삭제'},
