7922f9a4e213047be2877a498c91a2f179366535
aldegad <aldegad@naver.com>
2021-12-13 22:07:30 +0900

    ui guide end

--- src/app/basic/component/ui/row/row.component.ts
+++ src/app/basic/component/ui/row/row.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ContentChildren, HostBinding, Input, OnDestroy, OnInit, QueryList } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, ElementRef, HostBinding, Input, OnDestroy, OnInit, QueryList } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CalendarComponent } from '../../input/calendar/calendar.component';
 import { InputComponent } from '../../input/input/input.component';
@@ -18,8 +18,13 @@ export class RowComponent implements OnInit {
 
   @Input() horizontal:'top' | 'bottom' | 'center' | 'stretch';
   @Input() vertical:'top' | 'bottom' | 'center' | 'stretch';
+  @Input() set padding(_padding:string | number) {
+    this.el.nativeElement.style.setProperty('--app-padding', _padding);
+  }
 
-  constructor() { }
+  constructor(
+    private el: ElementRef
+  ) { }
 
   ngOnInit() {}
 }
