7307c3c51f45883c58fe33745ad3d7fa01a5193f
aldegad <aldegad@naver.com>
2022-01-05 15:58:34 +0900

    bug fix

--- src/app/basic/service/core/user.service.ts
+++ src/app/basic/service/core/user.service.ts
@@ -6,10 +6,10 @@ enum TAG {
   UserData = 'Devmonster@UserData'
 }
 
-export type UserType = "WORKER" | "PARTNER";
+export type UserType = 'WORKER' | 'LH' | 'SUPER' | 'COMPANY';
 export type UserGender = "남" | "여";
 export type RequestState = "승인" | "대기";
-export interface userAuthToken {
+export interface AuthToken {
   login_token:string;
   refresh_token:string;
 }
@@ -21,22 +21,36 @@ export class UserData {
 })
 export class UserService {
 
+  authToken:AuthToken;
   userData:UserData = new UserData();
   autoLogin:boolean = false;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId
   ) {
-    
+    this.getAuthToken();
   }
   getAuthToken() {
+    if(isPlatformServer(this.platformId)) return this.authToken = null; // ssr에서 auth token을 어떠헥 처리해야 하지? 쿠키로 해야하나? 아니면 정보를 안띄워야 하나?
 
+    let storage:Storage;
+    if(window.sessionStorage.getItem(TAG.AuthToken)) {
+      storage = window.sessionStorage;
+      this.autoLogin = false;
+    } else if(window.localStorage.getItem(TAG.AuthToken)) {
+      storage = window.localStorage;
+      this.autoLogin = true;
+    }
+    if(storage) { this.authToken = JSON.parse(storage.getItem(TAG.AuthToken)) }
+    else this.authToken = null;
   }
-  setAuthToken(authToken:userAuthToken, autoLogin:boolean = true) {
+  setAuthToken(authToken:AuthToken, autoLogin:boolean = true) {
     if(isPlatformServer(this.platformId)) return;
 
     let storage:Storage = autoLogin ? window.localStorage : window.sessionStorage;
     storage.setItem(TAG.AuthToken, JSON.stringify(authToken));
+
+    this.getAuthToken();
   }
   getUserData() {
     if(isPlatformServer(this.platformId)) return this.userData = new UserData();
@@ -49,9 +63,6 @@ export class UserService {
       storage = window.localStorage;
       this.autoLogin = true;
     }
-  setAuthToken(rsObj: any) {
-    throw new Error('Method not implemented.');
-  }
     if(storage) { this.userData = JSON.parse(storage.getItem(TAG.UserData)) }
     else this.userData = new UserData();
   }
