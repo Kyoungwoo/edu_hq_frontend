029e71d19ab47205c7edab11a11c8c8b61d9d43b
hyewon1234 <heywon303@naver.com>
2022-01-20 10:23:30 +0900

    commit

--- src/app/component/modal/search-scene/search-scene.component.ts
+++ src/app/component/modal/search-scene/search-scene.component.ts
@@ -41,8 +41,4 @@ export class SearchSceneComponent implements OnInit {
       this.res = await this.connect.run('/category/certify/search_my_project/get', this.form, { loading: '현장 검색' });
     }
   }
-
-  select() {
-    this._modal.dismiss(this.selectedItem);
-  }
 }
