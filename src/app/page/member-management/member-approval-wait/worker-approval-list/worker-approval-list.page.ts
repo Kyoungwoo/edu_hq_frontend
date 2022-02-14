4a609ddb57c1381a358adcb2c19be4c774efbe93
hyewon1234 <heywon303@naver.com>
2022-02-14 17:54:10 +0900

    commit

--- src/app/page/member-management/member-approval-wait/worker-approval-list/worker-approval-list.page.ts
+++ src/app/page/member-management/member-approval-wait/worker-approval-list/worker-approval-list.page.ts
@@ -1,7 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { ConnectResult, ConnectService } from 'src/app/basic/service/core/connect.service';
+import { UserService } from 'src/app/basic/service/core/user.service';
+import { ToastService } from 'src/app/basic/service/ionic/toast.service';
+import { DateService } from 'src/app/basic/service/util/date.service';
+import { ApprovalPopupComponent } from '../approval-popup/approval-popup.component';
+import { SecurityPasswordComponent } from '../security-password/security-password.component';
 import { WorkerApprovalEditPage } from '../worker-approval-edit/worker-approval-edit.page';
 
+class WorkerInfo {
+  ctgo_construction_id: number;
+  create_user_id: number;
+  company_id: number;
+  user_name: string;
+  ctgo_construction_name: string;
+  ctgo_country_name_kr: string;
+  update_date: string;
+  update_user_id: number;
+  user_id: number;
+  company_name: string;
+  ctgo_country_id: number;
+  approval_state: string;
+  create_date: string;
+  user_health: string;
+  row_count: number;
+}
 @Component({
   selector: 'app-worker-approval-list',
   templateUrl: './worker-approval-list.page.html',
@@ -9,17 +32,88 @@ import { WorkerApprovalEditPage } from '../worker-approval-edit/worker-approval-
 })
 export class WorkerApprovalListPage implements OnInit {
 
+  form = {
+    approval_states: [],
+    company_ids: [2],
+    ctgo_construction_ids: [],
+    end_date: this.date.today(),
+    project_ids: [],
+    search_text: '',
+    start_date: this.date.today({ month: -1 }),
+    limit_no: 0
+  };
+
+  res:ConnectResult<WorkerInfo>;
+
+  selectedList = [];
   constructor(
-    private modal : ModalController
+    private user: UserService,
+    private modal : ModalController,
+    private connect: ConnectService,
+    private date: DateService,
+    private toast: ToastService
   ) { }
 
   ngOnInit() {
+    this.get();
   }
 
-  async edit() {
+  async get(limit_no = this.form.limit_no) {
+
+    this.form.limit_no = limit_no;
+    const authToken = this.user.memberAuthToken;
+    //this.form.auth_token = authToken;
+
+    this.res = await this.connect.run('/approval/worker/list', this.form, {
+      loading: true
+    });
+    if(this.res.rsCode === 0) {
+      // 정상
+    } else if(this.res.rsCode === 1008) {
+      // 데이터 없음
+    }
+    else if(this.res.rsCode === 3000) {
+      // 비밀번호 없거나 틀렸음
+      this.getPassword();
+    } else {
+      // 그외. 인터넷안됨, 서버연결안됨 등등
+    }
+  }
+
+  async getPassword() {
+    const modal = await this.modal.create({
+      component: SecurityPasswordComponent,
+    });
+    const { data } = await modal.onDidDismiss();
+    if(data) {
+      this.get();
+    }
+  }
+
+  async edit(item) {
     const modal = await this.modal.create({
       component:WorkerApprovalEditPage,
+      componentProps:{
+        item
+      }
     });
     modal.present();
+  }
+
+  async approval() {
+    if(!this.selectedList.length) return await this.toast.present({message:'작업자를 선택해주세요.', color:'danger'})
+    const modal = await this.modal.create({
+      component:ApprovalPopupComponent,
+      componentProps:{
+        selectedList:this.selectedList
+      },
+      cssClass:"approval-modal"
+    });
+    modal.present();
+
+    const { data } = await modal.onDidDismiss();
+    if(data) {
+      this.get();
+    }
   }
 }
