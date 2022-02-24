29b228ebe469be70ca093903e4c558319d79f692
rlawnsxo <wnsxo8913@naver.com>
2022-02-24 09:53:56 +0900

    commit

--- src/app/page/today-work/my-status-situation/worker-status-add/worker-status-add.page.ts
+++ src/app/page/today-work/my-status-situation/worker-status-add/worker-status-add.page.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { async } from '@angular/core/testing';
 import { ModalController } from '@ionic/angular';
 import { ConnectResult, ConnectService } from 'src/app/basic/service/core/connect.service';
 import { UserService } from 'src/app/basic/service/core/user.service';
+import { AlertService } from 'src/app/basic/service/ionic/alert.service';
+import { ToastService } from 'src/app/basic/service/ionic/toast.service';
 
 export class ctgoMemberItem {
   user_id: number;
@@ -22,15 +25,23 @@ export class WorkerStatusAddPage implements OnInit {
   @Input() project_id:number;
   @Input() select_type:string;
   @Input() area_risk_id:number;
+  @Input() areadata:any;
   
   form = {
     // master_company_id: this.user.userData.belong_data.company_id,
-    master_company_id: 12,
-    project_id: 0,
+    master_company_id: 2,
+    project_id: 1,
     search_text:'',
     select_type:'',
     user_type:'',
-    area_risk_id:0
+    area_risk_id:0,
+    area_bottom_id:0,
+    area_middle_id:0,
+    area_top_id:0,
+    insert_state:'',
+    inside_time:'',
+    serial_type:'',
+    user_ids:[]
   }
 
   res:ConnectResult<ctgoMemberItem>
@@ -39,11 +50,13 @@ export class WorkerStatusAddPage implements OnInit {
   constructor(
     private _modal : ModalController,
     private connect: ConnectService,
-    private user: UserService
+    private user: UserService,
+    private alert: AlertService,
+    private toast: ToastService
   ) { }
 
   ngOnInit() {
-    console.log(this.select_type);
+    
     if(this.area_risk_id) {
       this.riskGet()
     } else {
@@ -69,6 +82,29 @@ export class WorkerStatusAddPage implements OnInit {
     if(!this.selectData.includes(item)) this.selectData.push(item);
   }
   async workerIn() {
+    this.selectData.map(item => this.form.user_ids.push(item.user_id));
+    this.form.area_risk_id ? this.form.user_type = '위험지역' : this.form.user_type = '게이트'
+    
+    this.form = {
+      ...this.areadata,
+      ...this.form
+    }
+    console.log("this.=form",this.form);
+    const alert = await this.alert.present({
+      message:'선택한 인원을 입장 처리하시겠습니까?',
+      buttons:[
+        {text:'아니요'},
+        {text:'예',
+          handler:async() => {
+            const res = await this.connect.run('/work_project/nfc_beacon/manual/insups',this.form);
+            if(res.rsCode === 0) {
+              this.toast.present({message:`${this.selectData.length}명이 입장 처리되엇습니다.`})
+              this._modal.dismiss(true);
+            }
 
+          }
+        }
+      ]
+    })
   }
 }
