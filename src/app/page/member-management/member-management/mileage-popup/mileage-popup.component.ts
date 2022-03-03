1f765014cd68aa0ca45bf4c16e9136894c3b7a08
hyewon1234 <heywon303@naver.com>
2022-03-03 20:28:28 +0900

    member

--- src/app/page/member-management/member-management/mileage-popup/mileage-popup.component.ts
+++ src/app/page/member-management/member-management/mileage-popup/mileage-popup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ConnectService } from 'src/app/basic/service/core/connect.service';
+import { UserService } from 'src/app/basic/service/core/user.service';
+import { AlertService } from 'src/app/basic/service/ionic/alert.service';
+import { NavService } from 'src/app/basic/service/ionic/nav.service';
 
 @Component({
   selector: 'app-mileage-popup',
@@ -7,8 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MileagePopupComponent implements OnInit {
 
-  constructor() { }
+  form = {
+    user_id: this.user.userData.user_id,
+    approval_user_ids: [],
+    approval_text: '',
+    user_name: [],
+    session_company_id: this.user.userData.belong_data.company_id,
+    user_manage_session: this.user.memberAuthToken
+
+  }
+
+  constructor(
+    private _modal: ModalController,
+    private connect: ConnectService,
+    private nav: NavService,
+    private user: UserService,
+    private alert: AlertService
+  ) { }
 
   ngOnInit() {}
+
 
 }
