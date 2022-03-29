d37b139ba8af1de8570ea9e27c8081949e1efbe7
aldegad <aldegad@naver.com>
2022-03-30 07:54:18 +0900

    appId add

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.devmonster.gsil.lh.skeeper',
   appName: 'ionic-angulart-starter',
   webDir: 'www',
   bundledWebRuntime: false
