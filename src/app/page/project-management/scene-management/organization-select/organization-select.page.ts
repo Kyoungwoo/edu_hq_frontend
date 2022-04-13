ac491f714fb87851f5d77861a7047d3010cf2db9
hyewon1234 <heywon303@naver.com>
2022-04-13 14:58:46 +0900

    commit

--- src/app/page/project-management/scene-management/organization-select/organization-select.page.ts
+++ src/app/page/project-management/scene-management/organization-select/organization-select.page.ts
@@ -1,73 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { listAnimation } from 'src/app/basic/basic.animation';
 import { ConnectResult, ConnectService } from 'src/app/basic/service/core/connect.service';
+
+class RegionalItem {
+  hq_regional_code: string;
+  hq_regional_entire_state: number;
+  hq_regional_id: number;
+  hq_regional_name: string;
+}
+
+class BusinessItem{
+  hq_business_code: string;
+  hq_business_entire_state: number;
+  hq_business_id: number;
+  hq_business_name: string;
+  hq_regional_id: number;
+}
 
 @Component({
   selector: 'app-organization-select',
   templateUrl: './organization-select.page.html',
   styleUrls: ['./organization-select.page.scss'],
+  animations: [ listAnimation ]
 })
 export class OrganizationSelectPage implements OnInit {
 
-  businessItem:ConnectResult<{
-    hq_business_name: string,
-    hq_regional_id: number,
-    hq_business_code: string,
-    hq_business_id: number
-    checked:boolean
-  }>
+  regionalItem:ConnectResult<RegionalItem>;
+  businessItem:ConnectResult<BusinessItem>;
+  level1selectedItem: RegionalItem;
+  level2selectedItem: BusinessItem;
 
-  regionalItem:ConnectResult<{
-    hq_regional_id: number,
-    hq_regional_name: string,
-    hq_regional_code: string
-    checked:boolean
-  }>
   retrunData = {
-    regName:'',
-    regId:0,
-    busName:'',
-    busId:0
+    regName: '',
+    regId: 0,
+    busName: '',
+    busId: 0
   }
+ 
   constructor(
-    private connect:ConnectService,
-    private _modal:ModalController
+    private connect: ConnectService,
+    private _modal: ModalController
   ) { }
 
   ngOnInit() {
     this.regional();
   }
-  async business(item) {
-    this.regionalItem.rsMap.forEach(data =>{
-      if(item.hq_regional_id === data.hq_regional_id){
-        data.checked = true;
-        this.retrunData.regName = item.hq_regional_name;
-        this.retrunData.regId = item.hq_regional_id;
-
-      } else {
-        data.checked = false;
-      }
-    })
-    this.businessItem = await this.connect.run('/category/organization/business/get',{hq_regional_id:item.hq_regional_id})
-    if(this.businessItem.rsCode === 0) {}
-  }
   async regional(){
     this.regionalItem = await this.connect.run('/category/organization/regional/get');
     if(this.regionalItem.rsCode === 0) {}
   }
-  checkChange(item) {
-    this.businessItem.rsMap.forEach(data => {
-      if(item.hq_business_id === data.hq_business_id){
-        data.checked = true;
-        this.retrunData.busName = item.hq_business_name;
-        this.retrunData.busId = item.hq_business_id;
-      } else { 
-        data.checked = false;
-      }
-    });
+
+  async business(item) {
+    this.level1selectedItem = item;
+    this.businessItem = await this.connect.run('/category/organization/business/get',{hq_regional_id:item.hq_regional_id})
+    if(this.businessItem.rsCode === 0) {}
   }
 
   submit(){
-    this._modal.dismiss(this.retrunData);
+    this._modal.dismiss({
+      
+    });
   }
 }
