7a7dda6d8c0e1b2ddca95f5a34fbdc560bded8ba
stmake22 <stmake22@nate.com>
2022-05-24 16:23:54 +0900

    jihyuk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.devmonster.gsil.lh.skeeper',
-  appName: 'ionic-angulart-starter',
+  appName: '에스키퍼',
   webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
